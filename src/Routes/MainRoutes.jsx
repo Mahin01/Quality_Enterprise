abcd1fcea5313b34e48524e379be60bcac31f73e
Mahin01 <ashmahin4@gmail.com>
2025-10-04 14:34:33 +0600

    Header - Navigation Menu Added

---
+++ src/Routes/MainRoutes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Contact from "../Pages/Contact/Contact";
+
+ export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+    }
+])
